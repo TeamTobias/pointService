a63e8c1ef54ad44b522910946399a9ca0e0eb327
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-30 20:00:07 +0900

    도커 이미지 생성. read me 추가

--- src/test/java/com/mycompany/myapp/repository/MypointRepositoryTests.java
+++ src/test/java/com/mycompany/myapp/repository/MypointRepositoryTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
-@Profile("local")
+@Profile("test")
 public class MypointRepositoryTests {
 
     @Autowired
