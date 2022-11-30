42ed3ff343297f15aee14393b500d751e30b345d
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-30 18:11:56 +0900

    Revert "TEAM-157 포인트 사용.. 잘되는 거 확인"
    This reverts commit 262c8151b4c0c1d60d40320574b39990f3d24ec9.

--- src/main/java/com/mycompany/myapp/web/rest/MypointResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/MypointResource.java
@@ -51,6 +51,11 @@ public class MypointResource {
     @PostMapping("/v1")
     public ResponseEntity<MypointDTO> createMypoint(@RequestBody MypointDTO mypointDTO) throws URISyntaxException {
         log.debug("REST request to save Mypoint : {}", mypointDTO);
+        if (mypointDTO.getUserid() != null) throw new BadRequestAlertException(
+            "A new mypoint cannot already have an ID",
+            ENTITY_NAME,
+            "idexists"
+        );
         MypointDTO result = mypointService.save(mypointDTO);
         return ResponseEntity
             .created(new URI("/api/mypoints/" + result.getUserid()))
@@ -67,24 +72,82 @@ public class MypointResource {
         return ResponseEntity.status(HttpStatus.OK).body(mypointDTO);
     }
 
-    /*포인트 사용*/
-    @DeleteMapping("/v1")
-    public ResponseEntity<MypointDTO> updateMypoint(@RequestBody MypointDTO mypointDTO) throws URISyntaxException {
-        log.debug("REST request to save Mypoint : {}", mypointDTO);
+    @PutMapping("/v1/{id}")
+    public ResponseEntity<MypointDTO> updateMypoint(
+        @PathVariable(value = "id", required = false) final String id,
+        @RequestBody MypointDTO mypointDTO
+    ) throws URISyntaxException {
+        log.debug("REST request to update Mypoint : {}, {}", id, mypointDTO);
+        if (mypointDTO.getUserid() == null) throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+
+        if (!Objects.equals(id, mypointDTO.getUserid())) throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+
+        if (!mypointRepository.existsById(id)) throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+
         MypointDTO result = mypointService.update(mypointDTO);
+
         return ResponseEntity
-            .created(new URI("/api/mypoints/" + result.getUserid()))
-            .headers(HeaderUtil.createEntityCreationAlert(applicationName, false, ENTITY_NAME, result.getUserid()))
+            .ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, mypointDTO.getUserid()))
             .body(result);
     }
 
-    @PatchMapping("/v1")
+    //    /**
+    //     * {@code PATCH  /mypoints/:id} : Partial updates given fields of an existing mypoint, field will ignore if it is null
+    //     *
+    //     * @param id the id of the mypointDTO to save.
+    //     * @param mypointDTO the mypointDTO to update.
+    //     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated mypointDTO,
+    //     * or with status {@code 400 (Bad Request)} if the mypointDTO is not valid,
+    //     * or with status {@code 404 (Not Found)} if the mypointDTO is not found,
+    //     * or with status {@code 500 (Internal Server Error)} if the mypointDTO couldn't be updated.
+    //     * @throws URISyntaxException if the Location URI syntax is incorrect.
+    //     */
+    //    @PatchMapping(value = "/v1/{id}", consumes = { "application/json", "application/merge-patch+json" })
+    //    public ResponseEntity<MypointDTO> partialUpdateMypoint(
+    //        @PathVariable(value = "id", required = false) final String id,
+    //        @RequestBody MypointDTO mypointDTO
+    //    ) throws URISyntaxException {
+    //        log.debug("REST request to partial update Mypoint partially : {}, {}", id, mypointDTO);
+    //        if (mypointDTO.getId() == null) {
+    //            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+    //        }
+    //        if (!Objects.equals(id, mypointDTO.getId())) {
+    //            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+    //        }
+    //
+    //        if (!mypointRepository.existsById(id)) {
+    //            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+    //        }
+    //
+    //        Optional<MypointDTO> result = mypointService.partialUpdate(mypointDTO);
+    //
+    //        return ResponseUtil.wrapOrNotFound(
+    //            result,
+    //            HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, mypointDTO.getId())
+    //        );
+    //    }
+
+    @GetMapping("/v1")
     public ResponseEntity<List<MypointDTO>> getAllMypoints(@org.springdoc.api.annotations.ParameterObject Pageable pageable) {
         log.debug("REST request to get a page of Mypoints");
         Page<MypointDTO> page = mypointService.findAll(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
         return ResponseEntity.ok().headers(headers).body(page.getContent());
     }
+
+    //    /**
+    //     * {@code GET  /mypoints/:id} : get the "id" mypoint.
+    //     *
+    //     * @param id the id of the mypointDTO to retrieve.
+    //     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the mypointDTO, or with status {@code 404 (Not Found)}.
+    //     */
+    //    @GetMapping("/v1/{id}")
+    //    public ResponseEntity<MypointDTO> getMypoint(@PathVariable String id) {
+    //        log.debug("REST request to get Mypoint : {}", id);
+    //        Optional<MypointDTO> mypointDTO = mypointService.findOne(id);
+    //        return ResponseUtil.wrapOrNotFound(mypointDTO);
+    //    }
 
     @DeleteMapping("/v1/{id}")
     public ResponseEntity<Void> deleteMypoint(@PathVariable String id) {
